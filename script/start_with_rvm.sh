5dfdeaba782a24789eaef4c3d1acc4fd4e5fc81d
vlad <sumskyi@gmail.com>
2011-01-24 12:57:04 +0200

    startup comment

--- script/start_with_rvm.sh
+++ script/start_with_rvm.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# spawninig script for cherokee web-server (used as HTTP-proxy)
+# need that to use with RVM
+
 export HOME=/home/vlad
 export PATH=/home/vlad/bin:/usr/local/bin:/usr/bin:/bin:/usr/bin/X11:/usr/X11R6/bin:/usr/games:/usr/lib64/jvm/jre/bin:/home/vlad/.rvm/bin:/home/vlad/.rvm/bin
 [[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm"
